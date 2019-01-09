284aa85487d2795073ace3c93cc83a4489e57c38
alonlevko <alonlevko@gmail.com>
2019-01-09 01:03:08 -0800

    fujiyama

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include "FileCacheManager.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    FileCacheManager manager;
+    problem p("lish lash4");
+    solution s("ahhh5");
+    problem p1("lish lash6");
+    solution s1("ahhh7");
+    manager.saveSolution(p, s);
+    manager.saveSolution(p1, s1);
 }
